722c0961f8f5c3a964e22d6d42e271b8cde63916
Bonced <fon.de.bon@gmail.com>
2026-03-22 09:58:43 +0200

    lvlup

---
+++ hw_1/Task4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 20;
+            int b = 4;
+
+
+            int sum = a + b;
+            int mult = a * b;
+            int div = a / b;
+            int sub = a - b;
+
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Product: {mult}");
+            Console.WriteLine($"Quotient: {div}");
+            Console.WriteLine($"Difference: {sub}");
+
+        }
+    }
+}
